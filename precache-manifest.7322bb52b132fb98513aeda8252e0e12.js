a7f6e995bd8ed1216e5cd2a67d14491d14d019bb
rdsingh120 <82333889+rdsingh120@users.noreply.github.com>
2022-05-28 11:31:40 -0300

    Updates

---
+++ precache-manifest.7322bb52b132fb98513aeda8252e0e12.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5da5ea7f9cd54a4493f2",
+    "url": "/robofriends/static/css/main.c6a8a33e.chunk.css"
+  },
+  {
+    "revision": "5da5ea7f9cd54a4493f2",
+    "url": "/robofriends/static/js/main.c8bc2d4e.chunk.js"
+  },
+  {
+    "revision": "143eb19aa518302daf32",
+    "url": "/robofriends/static/js/runtime~main.fb41d28d.js"
+  },
+  {
+    "revision": "b6d8db487209594c1188",
+    "url": "/robofriends/static/js/2.ef545e1a.chunk.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "f808190a4b12413b45f49d4bc1235858",
+    "url": "/robofriends/index.html"
+  }
+];
